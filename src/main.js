63e432ac91e627d7640d318479832689657db6f8
zxd <3277924430@qq.com>
2022-02-25 22:08:01 +0800

    完成了商品分类所有

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/global.css'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor)
 
 
 Vue.component('tree-table', ZkTable)
+
+Vue.filter('dateFormat',function(originVal){
+  const dt =new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth()+1+'').padStart(2,'0')
+  const d = (dt.getDate()+'').padStart(2,'0')
+  const hh = (dt.getHours()+'').padStart(2,'0')
+  const mm = (dt.getMinutes()+'').padStart(2,'0')
+  const ss = (dt.getSeconds()+'').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 Vue.config.productionTip = false
 
